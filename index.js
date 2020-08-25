07e475c9824357f97a8a17d495d43d047038be12
André Davis <davis.andre@gmail.com>
2020-08-25 19:49:21 +0000

    Break

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ module.exports = {
 
 console.log('Sample Repl.It project attached to GitHub');
 console.log('This is a JavaScript sample');
+
+debugger;
+console.log(typeof stringBuilder);
+
+/*
+stringBuilder.appendLine('First line added with appendLine.');
+stringBuilder.append('This line was added with append');
+stringBuilder.append('\n\n\n');
+
+console.log(stringBuilder.toString());
+*/
